2901223ce839ee692dc333508743a44bc6ce91d7
LaurHalati <82648298+LaurHalati@users.noreply.github.com>
2021-04-16 12:58:49 +0300

    Update DroneController.java

--- Java/DroneController.java
+++ Java/DroneController.java
@@ -19,11 +19,10 @@ public class DroneController {
     DroneController(DroneRepository repository){
         this.repository = repository;
     }
-    //@CrossOrigin(origins = "*")
     @GetMapping(value = "/drones")
-    DronePlan all(){
+    List<DronePlan> all(){
 
-        return (DronePlan) repository;
+        return (List<DronePlan>)repository.findAll();
     }
 
 
@@ -31,31 +30,7 @@ public class DroneController {
     @PostMapping( value ="/drones",consumes = "application/json")
     DronePlan  newDronePlan (@RequestBody DronePlan newDronePlan) {
         System.out.println(newDronePlan.toString());
-//        double[][] d = newDronePlan.getCoordinates();
-//        String[] parts = Arrays.deepToString(d).split(",");
-//        parts[0] = parts[0].substring(2,parts[0].length());
-//        parts[parts.length-1]= parts[parts.length-1].substring(0, parts[parts.length-1].length()-2);
-//
-//        for (int i=1;i<parts.length-2;i+=2) {
-//                parts[i] = parts[i].substring(0, parts[i].length() - 1);
-//                parts[i+1] = parts[i+1].substring(2);
-//
-//        }
-//
-//        double coordonate [][] = new double [parts.length/2][2];
-//        for( int i=0;i<parts.length;i++){
-//            int j=i/2;
-//            if(i%2==0)
-//                coordonate[j][0]= Double.parseDouble(parts[i]);
-//            else
-//                coordonate[j][1]= Double.parseDouble(parts[i]);
-//        }
-//        for (int i =0 ;i<coordonate.length;i++) {
-//            for (int j = 0; j < 2; j++){
-//                System.out.print(coordonate[i][j]+" ");
-//        }
-//            System.out.println(" ");
-//        }
+
 
 
         ObjectMapper mapper = new ObjectMapper();
