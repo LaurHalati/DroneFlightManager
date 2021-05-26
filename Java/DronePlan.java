39e36d5a282d4f64c0e86c7cf1ce73d58538a112
LaurHalati <82648298+LaurHalati@users.noreply.github.com>
2021-05-26 20:58:46 +0300

    Update DronePlan.java

--- Java/DronePlan.java
+++ Java/DronePlan.java
@@ -1,35 +1,47 @@
 package com.example.demo;
 
-import org.springframework.data.elasticsearch.annotations.Document;
 
-import javax.persistence.*;
-import java.lang.annotation.Documented;
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.data.elasticsearch.annotations.*;
+import org.springframework.data.elasticsearch.core.geo.GeoJsonPolygon;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
 import java.util.Objects;
 
-@Document(indexName="name")
+
+@Document(indexName = "drone-plan-laurentiu")
 public class DronePlan {
-    private @Id @GeneratedValue Long id;
+    @Id
+    @GeneratedValue
+    private String id;
     private String name;
-    private String geometryType;
-    @ElementCollection()
-    private List<List<Double>> coordinates;
-    public DronePlan(){
+    @GeoShapeField
+    private GeoJsonPolygon geometry;
+    @Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd'T'HH:mm")
+    private Date startTime;
+    @Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd'T'HH:mm")
+
+    private Date endTime;
+
+    public DronePlan() {
 
     }
-    public DronePlan(Long id, String name, String geometryType, List<List<Double>> coordinates) {
+
+    public DronePlan(String id, String name, GeoJsonPolygon geometry, Date startTime, Date endTime) {
         this.id = id;
         this.name = name;
-        this.geometryType = geometryType;
-        this.coordinates = coordinates;
+        this.geometry = geometry;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
-    public Long getId() {
+
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -41,30 +53,28 @@ public class DronePlan {
         this.name = name;
     }
 
-    public String getGeometryType() {
-        return geometryType;
+    public GeoJsonPolygon getGeometry() {
+        return geometry;
     }
 
-    public void setGeometryType(String geometryType) {
-        this.geometryType = geometryType;
+    public void setGeometry(GeoJsonPolygon geometry) {
+        this.geometry = geometry;
     }
 
-    public List<List<Double>> getCoordinates() {
-        return coordinates;
+    public Date getStartTime() {
+        return startTime;
     }
 
-    public void setCoordinates(List<List<Double>> coordinates) {
-        this.coordinates = coordinates;
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
     }
 
-    @Override
-    public String toString() {
-        return "DronePlan{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", geometryType='" + geometryType + '\'' +
-                ", coordinates=" + coordinates +
-                '}';
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
     }
 
     @Override
@@ -72,11 +82,22 @@ public class DronePlan {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         DronePlan dronePlan = (DronePlan) o;
-        return Objects.equals(id, dronePlan.id) && Objects.equals(name, dronePlan.name) && Objects.equals(geometryType, dronePlan.geometryType) && Objects.equals(coordinates, dronePlan.coordinates);
+        return Objects.equals(id, dronePlan.id) && Objects.equals(name, dronePlan.name) && Objects.equals(geometry, dronePlan.geometry) && Objects.equals(startTime, dronePlan.startTime) && Objects.equals(endTime, dronePlan.endTime);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, geometryType, coordinates);
+        return Objects.hash(id, name, geometry, startTime, endTime);
+    }
+
+    @Override
+    public String toString() {
+        return "DronePlan{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", geometry=" + geometry +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
     }
 }
